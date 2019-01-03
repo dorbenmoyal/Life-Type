14e6572a10f5dff4f141b0207a920f68401a16ee
Dor Ben Moyal <dor@ngworkshop.co.il>
2019-01-03 09:29:51 +0200

    no message

--- src/server.js
+++ src/server.js
@@ -1,13 +1,5 @@
 let express = require('express'),
-    path = require('path');
+path = require('path');
 var app = express();
 let server = require('http').Server(app);
 var port = process.env.PORT || 8000;
-
-app.use(express.static(path.join(__dirname)));
-
-
-
-server.listen(port, () => {
-    console.log("App is running on port " + port);
-});
