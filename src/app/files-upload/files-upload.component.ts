1a21b1821bd8d339912aee4c2c966439a765736c
Dor Ben Moyal <dor@ngworkshop.co.il>
2019-01-21 09:09:52 +0200

    Files Errors

--- src/app/files-upload/files-upload.component.ts
+++ src/app/files-upload/files-upload.component.ts
@@ -1,49 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 
+
 const URL = 'http://localhost:3000/drive/uploadFile';
 
 @Component({
   selector: 'app-files-upload',
   templateUrl: './files-upload.component.html',
-  styleUrls: ['./files-upload.component.css']
+  styleUrls: ['./files-upload.component.css'],
+  providers : []
 })
 export class FilesUploadComponent implements OnInit {
 
   public uploader: FileUploader = new FileUploader({url: URL, itemAlias: 'photo'});
 
-  public files  : any[];
+   static filesQ  : any [] = [];
   isHTML5 = true;
+  static compleateUpload = false;
   
   constructor() {
-    this.files = new Array();
+    
+   }
+
+   get FilesQ(){
+     return FilesUploadComponent.filesQ;
    }
 
   ngOnInit() {
    
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false;  };
     this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+
+      FilesUploadComponent.compleateUpload = false;
       
          console.log('ImageUpload:uploaded:', item, status, response);
           if(status == "200"){
             console.log("GOT IT")
           }
           else{
-            var fileError = {status : "fail",fileName : item.fileName};
-            this.files.push(fileError);
+            var fileError = {status : "fail",fileName : item.file.name};
+            FilesUploadComponent.filesQ.push(fileError);
+            
           }
         // this.files.push(item);
      };
      this.uploader.onCompleteAll = function compleate(){
 
-      console.log("compleated!!!@!#@#$%");
-      console.log(this.files);
-      this.files.forEach(element => {
-        
+      FilesUploadComponent.filesQ.forEach(element => {
+        FilesUploadComponent.compleateUpload = true;
+
         console.log(element);
       });
-
      };
+
+     
+ 
 
   }
 
